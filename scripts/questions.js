db986d7f5ebf9d46683c13a29555f1793b012ac5
gurranb <fnuf_links@hotmail.com>
2024-04-11 09:18:23 +0200

    questions

--- scripts/questions.js
+++ scripts/questions.js
@@ -7,96 +7,96 @@ export const questions = [
     image: "images/hippo.jpg",
     answers: ["Elefant", "Flodhäst", "Tiger"],
     correctAnswer: "Flodhäst",
-    points: 3
+    points: 2
   },
   {
     name: "Fråga 2",
     latitude: 58.752424,
     longitude: 17.006624,
-    question: "vad visas på bildennnnn?",
-    image: "images/hippo.jpg",
-    answers: ["Elefant", "Flodhäst", "Tiger"],
-    correctAnswer: "Flodhäst",
-    points: 3
+    question: "Vad är huvudstaden i Sverige?",
+    image: "images/city.jpg",
+    answers: ["Stockholm", "Göteborg", "Malmö"],
+    correctAnswer: "Stockholm",
+    points: 2
   },
   {
     name: "Fråga 3",
     latitude: 58.752107,
     longitude: 17.005241,
-    question: "vad visas på bilden?",
-    image: "images/hippo.jpg",
-    answers: ["Elefant", "Flodhäst", "Tiger"],
-    correctAnswer: "Flodhäst",
-    points: 3
+    question: "Vilket djur sägs ha nio liv?",
+    image: "images/dogcatcow.jpg",
+    answers: ["Hund", "Katt", "Ko"],
+    correctAnswer: "Katt",
+    points: 2
   },
   {
     name: "Fråga 4",
     latitude: 58.751706,
     longitude: 17.003561,
-    question: "vad visas på bilden?",
-    image: "images/hippo.jpg",
-    answers: ["Elefant", "Flodhäst", "Tiger"],
-    correctAnswer: "Flodhäst",
-    points: 3
+    question: "Vad är 4*8?",
+    image: "images/math.png",
+    answers: ["48", "32", "24"],
+    correctAnswer: "32",
+    points: 2
   },
   {
     name: "Fråga 5",
     latitude: 58.751322,
     longitude: 17.002095,
-    question: "vad visas på bilden?",
-    image: "images/hippo.jpg",
-    answers: ["Elefant", "Flodhäst", "Tiger"],
-    correctAnswer: "Flodhäst",
-    points: 3
+    question: "Vad är huvudingrediensen i en hamburgare?",
+    image: "images/chef.png",
+    answers: ["Kyckling", "Köttfärs", "Fisk"],
+    correctAnswer: "Köttfärs",
+    points: 2
   },
   {
     name: "Fråga 6",
     latitude: 58.750429,
     longitude: 17.0029,
-    question: "vad visas på bilden?",
-    image: "images/hippo.jpg",
-    answers: ["Elefant", "Flodhäst", "Tiger"],
-    correctAnswer: "Flodhäst",
-    points: 3
+    question: "Vad är motsatsen till 'varmt'?",
+    image: "images/windy.png",
+    answers: ["Kallt", "Lugnt", "Vått"],
+    correctAnswer: "Kallt",
+    points: 2
   },
   {
     name: "Fråga 7",
     latitude: 58.750754,
     longitude: 17.004356,
-    question: "vad visas på bilden?",
-    image: "images/hippo.jpg",
-    answers: ["Elefant", "Flodhäst", "Tiger"],
-    correctAnswer: "Flodhäst",
-    points: 3
+    question: "Vilken färg har himlen på en klar dag?",
+    image: "images/sun.png",
+    answers: ["Grön", "Blå", "Röd"],
+    correctAnswer: "Blå",
+    points: 2
   },
   {
     name: "Fråga 8",
     latitude: 58.751183,
     longitude: 17.006063,
-    question: "vad visas på bilden?",
-    image: "images/hippo.jpg",
-    answers: ["Elefant", "Flodhäst", "Tiger"],
-    correctAnswer: "Flodhäst",
-    points: 3
+    question: "Vad är huvudingrediensen i en pizza Margherita?",
+    image: "images/pizza.jpg",
+    answers: ["Tomat", "Ost", "Oliver"],
+    correctAnswer: "Tomat",
+    points: 2
   },
   {
     name: "Fråga 9",
     latitude: 58.751517,
     longitude: 17.007405,
-    question: "vad visas på bilden?",
-    image: "images/hippo.jpg",
-    answers: ["Elefant", "Flodhäst", "Tiger"],
-    correctAnswer: "Flodhäst",
-    points: 3
+    question: "Vilken är världens största ocean?",
+    image: "images/ocean.jpg",
+    answers: ["Atlanten", "Stilla havet", "Indiska oceanen"],
+    correctAnswer: "Stilla havet",
+    points: 2
   },
   {
     name: "Fråga 10",
     latitude: 58.751915,
     longitude: 17.008825,
-    question: "vad visas på bilden?",
-    image: "images/hippo.jpg",
-    answers: ["Elefant", "Flodhäst", "Tiger"],
-    correctAnswer: "Flodhäst",
-    points: 3
+    question: "Vilken är den mest använda sökmotorn på internet?",
+    image: "images/search.jpg",
+    answers: ["Google", "Bing", "Yahoo"],
+    correctAnswer: "Google",
+    points: 2
   },
 ];
